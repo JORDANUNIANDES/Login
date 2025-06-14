c04f0d8c345cb225f7d2f1a1264b18e0fdd08b1c
JORDANUNIANDES <jordanqt94@uniandes.edu.ec>
2025-06-13 22:11:28 -0500

    Add files via upload

---
+++ Publishers.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Login.Vistas
+{
+    partial class Publishers
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dgvPublishers;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnEliminar;
+
+        private void InitializeComponent()
+        {
+            this.dgvPublishers = new System.Windows.Forms.DataGridView();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPublishers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPublishers
+            // 
+            this.dgvPublishers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top |
+                                        System.Windows.Forms.AnchorStyles.Bottom) |
+                                        System.Windows.Forms.AnchorStyles.Left) |
+                                        System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvPublishers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPublishers.Location = new System.Drawing.Point(12, 12);
+            this.dgvPublishers.Name = "dgvPublishers";
+            this.dgvPublishers.Size = new System.Drawing.Size(760, 400);
+            this.dgvPublishers.TabIndex = 0;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
+            this.btnAgregar.Location = new System.Drawing.Point(12, 420);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnEditar.Location = new System.Drawing.Point(350, 420);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 2;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            this.btnEliminar.Location = new System.Drawing.Point(697, 420);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // Publishers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dgvPublishers);
+            this.Name = "Publishers";
+            this.Text = "Publishers";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPublishers)).EndInit();
+            this.ResumeLayout(false);
+        }
+    }
---
+++ Titleauthor.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Login.Vistas
+{
+    partial class Titleauthor
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dgvTitleauthor;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnEliminar;
+
+        private void InitializeComponent()
+        {
+            this.dgvTitleauthor = new System.Windows.Forms.DataGridView();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTitleauthor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvTitleauthor
+            // 
+            this.dgvTitleauthor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top |
+                                        System.Windows.Forms.AnchorStyles.Bottom) |
+                                        System.Windows.Forms.AnchorStyles.Left) |
+                                        System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvTitleauthor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTitleauthor.Location = new System.Drawing.Point(12, 12);
+            this.dgvTitleauthor.Name = "dgvTitleauthor";
+            this.dgvTitleauthor.Size = new System.Drawing.Size(760, 400);
+            this.dgvTitleauthor.TabIndex = 0;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
+            this.btnAgregar.Location = new System.Drawing.Point(12, 420);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnEditar.Location = new System.Drawing.Point(350, 420);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 2;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            this.btnEliminar.Location = new System.Drawing.Point(697, 420);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // Titleauthor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dgvTitleauthor);
+            this.Name = "Titleauthor";
+            this.Text = "Titleauthor";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTitleauthor)).EndInit();
+            this.ResumeLayout(false);
+        }
+    }
